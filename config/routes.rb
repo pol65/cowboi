fe6db760abccc0cf2245eb0ea2b8bf61a7f4f889
Polo <paul.poitrenaud@gmail.com>
2020-01-16 15:28:22 +0100

    mailer added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'contact/new'
   root 'static_pages#landing'
   get 'static_pages/home'
+  get 'static_pages/contact'
   
   resources :portfolios do
     resources :flashs
   end
+
+  resources :contacts, only: [:new, :create]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
